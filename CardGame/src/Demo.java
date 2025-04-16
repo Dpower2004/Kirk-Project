8e044b57c2f7e0ac02a29f11afef4bd52c400ec3
LukeSoda <lukejsoda@gmail.com>
2025-04-16 02:35:40 -0400

    Update Demo.java

--- CardGame/src/Demo.java
+++ CardGame/src/Demo.java
@@ -1,11 +1,18 @@
 public class Demo {
     public static void main(String[] args) {
-        Player[] players = new Player[3];
-        players[0] = new Player("p1", 30, false);
-        players[1] = new Player("p2", 20, false);
-        players[2] = new Player("p3", 10, true);
-        CardGame game = new CardGame(players);
-        game.deal(3);
+        Player[] players = new Player[4];
+        players[3] = new HoldemPlayer(10000000, false, Blind.NONE);
+        players[0] = new HoldemPlayer(50, false, Blind.SMALL);
+        players[1] = new HoldemPlayer(150, true, Blind.BIG);
+        players[2] = new HoldemPlayer(60, false, Blind.NONE);
+        Holdem game = new Holdem(players, 50, 2);
+        game.setup();
+        System.out.println(game);
+        game.bettingRound();
+        System.out.println(game);
+        game.bettingRound();
+        System.out.println(game);
+        game.bettingRound();
         System.out.println(game);
     }
 }
