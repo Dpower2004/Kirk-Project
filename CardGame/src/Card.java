029c2c55e2b1557660106545ff92e111c420eb90
Luke Soda <luke@soda.fm>
2025-04-18 17:27:32 -0400

    final test

--- CardGame/src/Card.java
+++ CardGame/src/Card.java
@@ -1,6 +1,6 @@
 /**
  * Represents an individual card to be passed into the CardList class.
- * @author Luke Soda testwkejfw
+ * @author Luke Soda testwkejfwerfer
  * @version 1.01
  */
 public class Card {
